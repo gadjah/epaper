27f4262eaddd9ecd1f25310dea889c0385c6bd5b
langitmalam <langit.malam.4@gmail.com>
2012-06-24 15:40:48 +0800

    -s

--- kompas.py
+++ kompas.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 """
-__version__ = "$Revision: 0.7 $"
-__date__ = "$Date: 2010/02/24 $"
+__version__ = "$Revision: 0.8 $"
+__date__ = "$Date: 2012/06/24 $"
 """
 
 from PIL import Image
+from datetime import datetime
 import os
 import sys
 import re
@@ -13,7 +14,6 @@ import time
 import hashlib
 import zipfile
 import urllib2
-import datetime
 import optparse
 import cStringIO
 import cookielib
@@ -25,8 +25,8 @@ def main():
     cmd = optparse.OptionParser()
     cmd.add_option("-c", "--concurrent", dest="concurrent", type="int", default=1)
     cmd.add_option("-d", "--dir", dest="dir", default=web)
-    cmd.add_option("-g", "--guid", dest="guid", default="BC6B3C21-64AD-42EE-9DFB-8F693395B92D")
     cmd.add_option("-p", "--prefix", dest="filePrefix", default=web)
+    cmd.add_option("-s", "--date", dest="sdate", default="", help="YYYY-MM-DD")
     cmd.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False)
     cmd.add_option("-z", "--zip", action="store_true", dest="zip", default=False)
     (options, args) = cmd.parse_args()
@@ -39,38 +39,31 @@ def main():
     zip = options.zip
     dir = os.path.normpath(options.dir) + '/'
     computerID = ''
+    sdate = None
+
+    if options.sdate:
+        try: 
+            sdate = datetime.strptime(options.sdate,
+                   '%Y-%m-%d')
+        except ValueError:
+            log("Date does not match format YYYY-MM-DD")
+            sys.exit(1) 
+
     cookie = cookielib.CookieJar() 
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie), 
         urllib2.HTTPHandler(debuglevel=options.verbose))
-    opener.addheaders = [('User-Agent', 'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)')] 
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.5; rv:6.0.2) Gecko/20100101 Firefox/6.0.2')] 
     mainPage = "http://%s.realviewusa.com/?xml=%s.xml" % (web, web)
     log(mainPage)
-    request = urllib2.Request(mainPage)
-    cookie.set_cookie_if_ok(cookielib.Cookie(
-        version=0, 
-        name='RVKernel.User.UserGUID', 
-        value='%s' % (options.guid), 
-        port=None, 
-        port_specified=False, 
-        domain='.%s.realviewusa.com' % (web), 
-        domain_specified=True, 
-        domain_initial_dot=True, 
-        path='/', 
-        path_specified=True, 
-        secure=False, 
-        expires=None, 
-        discard=False, 
-        comment=None, 
-        comment_url=None, 
-        rest={}, 
-        rfc2109=False), 
-    request)  
-    page = opener.open(request)
+    page = opener.open(mainPage)
+
     for ck in cookie:
         if ck.name == 'computerid':
             computerID = ck.value
+
     html = page.read()
     xml = re.compile('<span class="teaserText"><a href="([^"]+)">([^<]+)</a></span>').findall(html)
+    #?!/
     del(xml[1:])
     for item in xml:
         if item[0] != mainPage:
@@ -86,42 +79,48 @@ def main():
         if not iid:
             log("iid=0")
             sys.exit(1)
-        
-        dDate = re.compile("i:'([^']+)'").findall(html)
-        if not dDate:
-            log("date=0")
-            sys.exit(1)     
-        
-        fDate = "%s-%s-%s" %(dDate[0][-4:], str(getMonth(dDate[0][-8:-5])), dDate[0][0:2])
+
         indexPage = "http://%s.realviewusa.com/global/loadconfig.aspx?fetch=2&i=&iguid=&xml&iid=%s&index=&rnd=0.1" % (web, iid[0])
         log(indexPage)
         page = opener.open(indexPage)
         html = page.read()
-        pageCount = re.compile('pagecount="(\d+)"').findall(html)
         
+        ss = re.findall('<BackIssue id="([^"]+)" sysname="([^"]+)" name="([^"]+)" path="([^"]+)" issuedate="([^"]+)', html)
+        if sdate:
+            iid = [] 
+            for s in ss:
+                if datetime.strptime(re.sub("\s{2,}", " ", s[4]), "%b %d %Y %H:%MAM").strftime("%Y-%m-%d") == sdate.strftime("%Y-%m-%d"): 
+                    iid.append(s[0])
+                    break
+            if not iid:
+                log("Date does not match")
+                sys.exit(1)
+            indexPage = "http://%s.realviewusa.com/global/loadconfig.aspx?fetch=2&i=&iguid=&xml&iid=%s&index=&rnd=0.1" % (web, iid[0])
+            page = opener.open(indexPage)
+            html = page.read()
+        else:
+            s = ss[0]
+        fdate =  datetime.strptime(s[4][7:11]+s[4][0:3]+("00" + s[4][4:6].strip())[-2:], "%Y%b%d").strftime("%Y-%m-%d")
+        stringDir = s[3]
+
+        pageCount = re.compile('pagecount="(\d+)"').findall(html)
         if not pageCount:
             log("pageCount=0")
             sys.exit(1) 
-        
-        stringDir = re.compile('path="([^"]+)"').findall(html)
-        
-        if not stringDir:
-            log("Dir=0")
-            sys.exit(1)
-            
-        Dir = re.sub("\s", '%20', stringDir[0])
+
+        Dir = re.sub("\s", '%20', stringDir)
         Url = "http://content.%s.realviewusa.com/djvu%s" % (web, Dir)
-        if not os.path.exists(dir + fDate):
-            os.makedirs(dir + fDate)
-        year = datetime.datetime.utcnow().year
-        month = datetime.datetime.utcnow().month
-        day = datetime.datetime.utcnow().day
-        hour = datetime.datetime.utcnow().hour
-        sd = '/djvu' + stringDir[0].lower()  
+        if not os.path.exists(dir + fdate):
+            os.makedirs(dir + fdate)
+        year = datetime.utcnow().year
+        month = datetime.utcnow().month
+        day = datetime.utcnow().day
+        hour = datetime.utcnow().hour
+        sd = '/djvu' + stringDir.lower()  
         threads = []
         s = threading.Semaphore(concurrent)
         for x in range(1, int(pageCount[0]) + 1):
-            outFile = '%s%s/%s_%s%s_%07d.jpg' % (dir, fDate, filePrefix, stringPage, fDate, x)  
+            outFile = '%s%s/%s_%s%s_%07d.jpg' % (dir, fdate, filePrefix, stringPage, fdate, x)  
             j = "/webimages/page%07d_large.jpg" % (x)
             n = "/webimages/page%07d_large.png" % (x)
             jHash = "%s%s%s%s%s%s%s" % (computerID, sd, j, year, month, day, hour)
@@ -135,8 +134,8 @@ def main():
             thread.join()
             
         if zip:
-            zipFile = "%s%s_%s%s.zip" % (dir, filePrefix, stringPage, fDate)
-            makezip(dir, fDate, filePrefix, stringPage, zipFile, xrange(1, int(pageCount[0]) + 1))
+            zipFile = "%s%s_%s%s.zip" % (dir, filePrefix, stringPage, fdate)
+            makezip(dir, fdate, filePrefix, stringPage, zipFile, xrange(1, int(pageCount[0]) + 1))
 
     log("\n-")
     
@@ -178,23 +177,21 @@ def downloader(opener, filename, s, jpg=None, png=None):
     finally:
         s.release()
         
-def makezip(dir, fDate, filePrefix, stringPage, filename, pageCount):
+def makezip(dir, fdate, filePrefix, stringPage, filename, pageCount):
     log("Create %s" % (filename)) 
     zip = zipfile.ZipFile(filename, mode="w", compression=zipfile.ZIP_DEFLATED) 
     for item in pageCount:
-        outFile = '%s%s/%s_%s%s_%07d.jpg' % (dir, fDate, filePrefix, stringPage, fDate, item)
+        outFile = '%s%s/%s_%s%s_%07d.jpg' % (dir, fdate, filePrefix, stringPage, fdate, item)
         try:
             zip.write(outFile)
         except OSError, e:
             log("Error %s, %s" % (e, filename))
     zip.close()
-        
+
 def log(str):
     print "%s >>> %s" % (time.strftime("%x - %X", time.localtime()), str)
-    
-def getMonth(month):
-    dict = {'Jan': "01", 'Feb': "02", 'Mar': "03", 'Apr': "04", 'May': "05", 'Jun': "06", 'Jul': "07", 'Aug': "08", 'Sep': "09", 'Oct': "10", 'Nov': "11", 'Dec': "12"}
-    return dict[month]
+
+
     
 if __name__ == '__main__':
     main()
